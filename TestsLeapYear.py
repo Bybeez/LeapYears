56558b8d9f210932176ac11bc61ad01b02dcdd97
Josué Foucaud <josue.foucaud@metapolis.fr>
2020-03-06 11:52:51 +0100

    test -> LeapYear: test if year multiple of 400 return true

--- TestsLeapYear.py
+++ TestsLeapYear.py
@@ -2,9 +2,14 @@ import unittest
 import LeapYear
 
 class LeapYearTests(unittest.TestCase):
-    def testAnyYearIsNotALeapYear(self):
+    def testIfNotALeapYearReturnFalse(self):
         year = 123
         expectedOutput = False
+        self.assertEqual(LeapYear.isLeapYear(year), expectedOutput)
+
+    def testIfYearDivisibleBy400ReturnTrue(self):
+        year = 400
+        expectedOutput = True
         self.assertEqual(LeapYear.isLeapYear(year), expectedOutput)
 
 if __name__=="__main__":
